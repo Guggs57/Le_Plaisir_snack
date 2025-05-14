b2abf5b63c52019e66fe4740778129f87665a297
Guggs57 <n_guggenbuhl@hotmail.com>
2025-05-14 21:31:36 +0200

    we can delete us profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_14_191737) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_14_192833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -67,7 +67,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_14_191737) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.decimal "total_price"
     t.string "status"
     t.datetime "created_at", null: false
