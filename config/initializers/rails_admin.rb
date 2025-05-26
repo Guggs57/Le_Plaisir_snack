aaf1d04063cb524f8fc552312779ae892101cc33
Guggs57 <n_guggenbuhl@hotmail.com>
2025-05-26 18:54:43 +0200

    update admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,32 +1,11 @@
 RailsAdmin.config do |config|
   config.asset_source = :sprockets
 
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == CancanCan ==
-  # config.authorize_with :cancancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/railsadminteam/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
+  # ... tes autres configs ...
 
   config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
+    dashboard
+    index
     new
     export
     bulk_delete
@@ -34,9 +13,19 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
+  end
 
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+  config.model 'Dish' do
+    # Ne pas afficher la méthode ingredients (qui n'est pas une association ActiveRecord)
+    exclude_fields :ingredients
+
+    # Champ virtuel affichant la liste des ingrédients par défaut
+    field :default_ingredients do
+      label 'Ingrédients par défaut'
+      pretty_value do
+        bindings[:object].ingredients.join(', ')
+      end
+      read_only true
+    end
   end
 end
